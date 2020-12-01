99c5d082618f81633f1b22735bcc5cbd5792a69a
Jameschy <chig@foxmail.com>
2020-12-01 17:15:57 +0800

    新建多个模块

---
+++ home/src/App.bak.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div id="app">
+        <Nav></Nav>
+
+    <img src="./assets/logo.png">
+    <router-view/>
+    <el1></el1>
+    <el2></el2>
+  </div>
+
+
+</template>
+
+<script>
+
+  import {el1,el2,Nav} from "./components";
+
+  export default {
+    name: 'App',
+
+    components:{
+      el1,
+      el2,
+      Nav
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ home/src/components/index.js
@@ -0,0 +1,14 @@
+/*
+*@name:index
+*@author:admins
+*@date:2020/12/1 16:03
+*@desc:admins
+*@update:2020/12/1 16:03
+*/
+export { default as el1} from './el1'
+export { default as el2} from './el2'
+export { default as el3} from './el3'
+export { default as Nav} from './Nav'
+export { default as SiteMsg} from './SiteMsg'
+export { default as Cont1} from './Cont1'
+export { default as Cont2} from './Cont2'
